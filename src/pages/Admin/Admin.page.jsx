5e9cd161125663efe39717d4ac03823fc3fbe931
bluejaygm <damato578@gmail.com>
2022-04-28 17:46:50 -0300

    remove cardinfo admin

--- src/pages/Admin/Admin.page.jsx
+++ src/pages/Admin/Admin.page.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import styles from './Admin.module.css';
 
 // Components
-import CardInfo from './../../components/Statistics/CardInfo/CardInfo.component';
 
 // Images
 import IncomeIcon from "./../../assets/icon_income.svg";
@@ -16,11 +15,6 @@ function Admin() {
       <header className={styles.header}>
         <h1 className={styles.title}>Squared Bread</h1>
       </header>
-      <section className={styles.cards}>
-        <CardInfo title="Entradas" content="R$ 20,00" image={IncomeIcon}/>
-        <CardInfo title="Saídas" content="R$ 20,00" image={ExpenseIcon}/>
-        <CardInfo title="Total" content="R$ 20,00" image={MoneyIcon}/>
-      </section>
     </section>
   );
 }
